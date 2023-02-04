210b713fef9712e098767069d4e67acc98d8a4c0
Anna Konieczka <konieczka.ania@gmail.com>
2023-02-04 13:54:29 +0100

    Finish drawing bottom rebar in cross section

--- cross_section_img.py
+++ cross_section_img.py
@@ -2,43 +2,121 @@ import cv2 as cv
 import numpy as np
 import os
 
-def cross_section_img():
+#BGR, nie RGB
+
+def cross_section_img(diameter11, diameter12, quantity11, quantity12, cover, diameter_s, height, width):
 
 
-	# diameter = 20
-	# diameter_s = 8
-	# cover = 30
+	height_mm = height*10
+	width_mm = width*10
+	img = np.zeros((height_mm, width_mm, 3), dtype=np.uint8)
 
-	a1x = cover + diameter_s + diameter/2
+	a1x = cover + diameter_s + diameter11/2
 	a1x = int(a1x)
 
-	#BGR, nie RGB
-
-	img = np.zeros((height*10, width*10, 3), dtype=np.uint8)
-
-
 	# cross section
-	cv.rectangle(img, (0,0), (width*10, height*10), (160,160,160), -1)
+	cv.rectangle(img, (0,0), (width_mm, height_mm), (160,160,160), -1)
 
 	# stirrup
-	cv.rectangle(img, (cover+int(diameter_s/2),cover+int(diameter_s/2)), (width*10-cover-int(diameter_s/2), height*10-cover-int(diameter_s/2)), (64,64,64), 8)
+	cv.rectangle(img, (cover+int(diameter_s/2),cover+int(diameter_s/2)), (width_mm-cover-int(diameter_s/2), height_mm-cover-int(diameter_s/2)), (64,64,64), 8)
 
 	# rebar
-	cv.circle(img, (a1x, a1x), int(diameter/2), (32,32,32), -1)
+	cv.circle(img, (a1x, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
 
 
-	
+	width_rebar_mm = width_mm - 2*cover - 2*diameter_s
+	rebar_spacing = int((width_rebar_mm - quantity11*diameter11 - quantity12*diameter12)/(quantity11 + quantity12 - 1))
+
+
+	# only first type of bars
+	if quantity12 == 0:
+
+		rebar_center = a1x + rebar_spacing + diameter11
+
+		for q in range(1,quantity11):
+
+			cv.circle(img, (rebar_center, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+			rebar_center = rebar_center + rebar_spacing + diameter11
+
+
+
+	# first + second type of bars
+	else:
+
+		a12x = cover + diameter_s + diameter12/2
+		a12x = int(a12x)
+
+		#left hand corner bar
+		cv.circle(img, (a1x, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+		#right hand corner bar
+		cv.circle(img, (width_mm-a1x, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+
+
+		if quantity11%2 == 1:
+
+			# rebar in the middle
+			cv.circle(img, (int(width_mm/2), height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+
+			rebar_center = a1x + rebar_spacing + diameter11
+
+			for q in range(1,int((quantity11-1)/2)):
+
+				cv.circle(img, (rebar_center, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+				cv.circle(img, (width_mm - rebar_center, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+				rebar_center = rebar_center + rebar_spacing + diameter11
+
+
+			rebar_center = int(rebar_center - 0.5*diameter11 + 0.5*diameter12)
+
+			for q in range(1,int(quantity12/2)+1):
+
+				cv.circle(img, (rebar_center, height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+				cv.circle(img, (width_mm - rebar_center, height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+				rebar_center = rebar_center + rebar_spacing + diameter12
+
+
+		if quantity11%2 == 0:
+
+			rebar_center = a1x + rebar_spacing + diameter11
+
+			for q in range(1,int(quantity11/2)):
+
+				cv.circle(img, (rebar_center, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+				cv.circle(img, (width_mm - rebar_center, height_mm-a1x), int(diameter11/2), (32,32,32), -1)
+				rebar_center = rebar_center + rebar_spacing + diameter11
+
+			rebar_center = int(rebar_center - 0.5*diameter11 + 0.5*diameter12)
+
+			if quantity12%2 == 0:
+
+				for q in range(1,int(quantity12/2)+1):
+
+					cv.circle(img, (rebar_center, height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+					cv.circle(img, (width_mm - rebar_center, height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+					rebar_center = rebar_center + rebar_spacing + diameter12
+
+
+			if quantity12%2 == 1:
+
+				#rebar in the middle
+				cv.circle(img, (int(width_mm/2), height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+
+				for q in range(1,int((quantity12-1)/2)+1):
+
+					cv.circle(img, (rebar_center, height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+					cv.circle(img, (width_mm - rebar_center, height_mm-a12x), int(diameter12/2), (32,32,32), -1)
+					rebar_center = rebar_center + rebar_spacing + diameter12	
+
+
 
 	path = 'C:\\Users\\konie\\OneDrive\\STUFF\\CODING\\01 ZAKOTWIENIE PRĘTÓW\\website\\static'
 
 	os.chdir(path)
 	cv.imwrite('concrete_cross_section.jpg', img)
 
-	# cv.imshow("concrete_cross_section", img)
-
-	# cv.waitKey(0)
-	# cv.destroyAllWindows()
 
 
+	# # cv.imshow("concrete_cross_section", img)
 
-
+	# # cv.waitKey(0)
+	# # cv.destroyAllWindows()
--- website/views_belka_zelbetowa.py
+++ website/views_belka_zelbetowa.py
@@ -3,9 +3,19 @@ from flask_login import login_required, current_user
 from . import db
 import json
 import math
-import cross_section_img
+from cross_section_img import cross_section_img
 
 views_belka_zelbetowa = Blueprint('views_belka_zelbetowa', __name__)
+
+
+strength_classes = ["C12/15", "C16/20", "C20/25", "C25/30", "C30/37", "C35/45", "C40/50", "C45/55", "C50/60",
+				"C55/67", "C60/75", "C70/85", "C80/95", "C90/105"]
+
+f_ck_dict = {"C12/15":12, "C16/20":16, "C20/25":20, "C25/30":25, "C30/37":30, "C35/45":35, "C40/50":40,
+			 "C45/55":45, "C50/60":50, "C55/67":55, "C60/75":60, "C70/85": 70, "C80/95": 80, "C90/105": 90}
+			 
+diameters = [6, 8, 10, 12, 16, 20, 25, 32, 40]
+
 
 
 
@@ -29,38 +39,11 @@ def belka_zelbetowa():
 			flash('Uzupełnij dane.')
 
 		else:
-			length = float(length)
-			q_def = float(q_def)
-			height = float(height)
-			width = float(width)
-
-
 			return redirect(url_for('views_belka_zelbetowa.belka_zelbetowa_02', static_scheme=static_scheme, length=length, q_def=q_def, height=height,
-									width=width))
-
-
-			# strength_class = request.form.get('strength_class')
-			# diameter = request.form.get('diameter')
-			# quantity = request.form.get('quantity')
-			# cover = request.form.get('cover')
-			# diameter_s = request.form.get('diameter_s')
-
-
-			# cross_section_img()
-
+								width=width))
 		
 	return render_template("belka_zelbetowa.html", user=current_user, static_scheme=static_scheme)
 
-
-
-
-strength_classes = ["C12/15", "C16/20", "C20/25", "C25/30", "C30/37", "C35/45", "C40/50", "C45/55", "C50/60",
-				"C55/67", "C60/75", "C70/85", "C80/95", "C90/105"]
-
-f_ck_dict = {"C12/15":12, "C16/20":16, "C20/25":20, "C25/30":25, "C30/37":30, "C35/45":35, "C40/50":40,
-			 "C45/55":45, "C50/60":50, "C55/67":55, "C60/75":60, "C70/85": 70, "C80/95": 80, "C90/105": 90}
-			 
-diameters = [6, 8, 10, 12, 16, 20, 25, 32, 40]
 
 
 @views_belka_zelbetowa.route('/belka_zelbetowa_02', methods=['GET', 'POST'])
@@ -74,8 +57,8 @@ def belka_zelbetowa_02():
 	static_scheme = request.args.get('static_scheme')
 	length = float(request.args.get('length'))
 	q_def = float(request.args.get('q_def'))
-	height = float(request.args.get('height'))
-	width = float(request.args.get('width'))
+	height = int(request.args.get('height'))
+	width = int(request.args.get('width'))
 
 
 	q_weight = 0.01*height*0.01*width*25
@@ -87,6 +70,62 @@ def belka_zelbetowa_02():
 	elif static_scheme == "obustronnie utwierdzona":
 		M_support = round(-(q_def+q_weight)*length**2/12, 3)
 		M_span = round((q_def+q_weight)*length**2/24, 3)
+
+
+	if request.method == 'POST':
+
+		strength_class = request.form.get('strength_class')
+		cover = request.form.get('cover')
+		diameter_s = request.form.get('diameter_s')
+
+		diameter11 = request.form.get('diameter11')
+		quantity11 = request.form.get('quantity11')
+		diameter12 = request.form.get('diameter12')
+		quantity12 = request.form.get('quantity12')
+		
+
+
+		if 0 in [len(cover)]:
+			flash('Uzupełnij dane.')
+
+		else:
+
+			cover = int(cover)
+			diameter_s = int(diameter_s)
+			diameter11 = int(diameter11)
+			quantity11 = int(quantity11)
+			diameter12 = int(diameter12)
+			quantity12 = int(quantity12)
+
+
+			if (quantity11%2 == 1 and quantity12%2 == 1):
+				flash('Liczby sztuk obu typów prętów nie mogą być obie nieparzyste (brak symetrii zbrojenia w przekroju).')
+
+
+# checking mimimum required rebar spacing
+
+			width_rebar_mm = width*10 - 2*cover - 2*diameter_s
+			rebar_spacing = (width_rebar_mm - quantity11*diameter11 - quantity12*diameter12)/(quantity11 + quantity12 - 1)
+
+			d_g = 16
+			k_1 = 1
+			k_2 = 5
+
+
+############################### warunki do przeanalizowania - kolejność zależna od średnicy pręta?
+
+			if rebar_spacing < min(diameter11, diameter12):
+				flash('Rozstaw prętów musi być większy niż największa z przyjętych średnic pręta.')
+
+			elif rebar_spacing < (d_g + k_2):
+				flash('Rozstaw prętów musi być większy niż średnica kruszywa powiększona o 5mm (przyjęta średnica kruszywa: 16mm).')
+
+			elif rebar_spacing < 20:
+				flash('Rozstaw prętów musi być większy niż 20mm.')
+
+			else:
+				cross_section_img(diameter11, diameter12, quantity11, quantity12, cover, diameter_s, height, width)
+
 
 
 
