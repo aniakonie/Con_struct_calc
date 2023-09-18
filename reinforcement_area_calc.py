ccb19371a9667f05f85631913396c7b5edd0c42a
Anna Konieczka <konieczka.ania@gmail.com>
2023-09-18 12:23:25 +0200

    commiting changes after a long break

--- reinforcement_area_calc.py
+++ reinforcement_area_calc.py
@@ -1,9 +1,11 @@
+import math
+
+
 def reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover, diameter_s, bottom_def_dia, top_def_dia):
 
 	M_ed = M_span
 	b = 0.01*width
 	h = 0.01*height
-
 
 	if des_sit == "trwała lub przejściowa":
 		gamma_c = 1.5
@@ -13,7 +15,6 @@ def reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover,
 		gamma_c = 1.2
 		gamma_s = 1.0
 
-
 	#concrete parameters
 	alpha = 1
 	f_cd = alpha*f_ck/gamma_c
@@ -22,14 +23,12 @@ def reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover,
 	f_yd = f_yk/gamma_s
 	E_s = 200000
 
-
 	if f_ck <= 50:
 		lambd = 0.8
 		eta = 1
 	if f_ck > 50:
 		lambd = 0.8 - (f_ck-50)/400
 		eta = 1 - (f_ck-50)/200
-
 
 	epsilon_cu3_dict = {"C12/15":0.0035, "C16/20":0.0035, "C20/25":0.0035, "C25/30":0.0035, "C30/37":0.0035, "C35/45":0.0035, "C40/50":0.0035,
 						 "C45/55":0.0035, "C50/60":0.0035, "C55/67":0.0031, "C60/75":0.0029, "C70/85": 0.0027, "C80/95": 0.0026, "C90/105": 0.0026}
@@ -46,25 +45,23 @@ def reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover,
 	A_0_lim = xi_lim*zeta_lim
 	A_0 = M_ed/(eta*f_cd*1000*b*d**2)
 
-
 	if A_0 <= A_0_lim:
 
 		#reinforcement for compression is not required
 		zeta = (1+((1-2*A_0)**(0.5)))/2
-		A_s1 = M_ed/(f_yd*1000*zeta*d)
-		A_s2 = 0
+		A_s1_req = M_ed/(f_yd*1000*zeta*d)
+		A_s2_req = 0
 
 	else:
 		#reinforcement for compression is required
 		M_Rd_lim = A_0_lim*eta*f_cd*1000*b*d**2
 		A_s1_1 = xi_lim*b*d*eta*f_cd/f_yd
 		A_s1_2 = (M_ed - M_Rd_lim)/f_yd*1000*(d - a_2)
-		A_s1 = A_s1_1 + A_s1_2
-		A_s2 = A_s1_2
+		A_s1_req = A_s1_1 + A_s1_2
+		A_s2_req = A_s1_2
 
-	A_s1 = 10000*A_s1
-	A_s2 = 10000*A_s2
-
+	A_s1_req = 10000*A_s1_req
+	A_s2_req = 10000*A_s2_req
 
 	f_cm = f_ck + 8
 
@@ -73,12 +70,157 @@ def reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover,
 	else:
 		f_ctm = 2.12*math.log(1+0.1*f_cm)
 
-
 	A_smin = max(0.26*f_ctm/f_yk*b*d, 0.0013*b*d)
 	A_smin = 10000*A_smin
 
 	A_smax = 0.04*b*h
 	A_smax = 10000*A_smax
 
+	return A_s1_req, A_s2_req, A_0, A_0_lim, A_smin, A_smax
 
-	return A_s1, A_s2, A_0, A_0_lim, A_smin, A_smax
+
+def bending_moment_resistance(M_span, width, height, des_sit, strength_class, f_ck, cover, diameter_s, diameter11, quantity11, diameter12, quantity12,
+								diameter11top, quantity11top, A_s1, A_s2):
+
+
+	M_ed = M_span
+	b = 0.01*width
+	h = 0.01*height
+
+	if des_sit == "trwała lub przejściowa":
+		gamma_c = 1.5
+		gamma_s = 1.15
+
+	if des_sit == "wyjątkowa":
+		gamma_c = 1.2
+		gamma_s = 1.0
+
+	#concrete parameters
+	alpha = 1
+	f_cd = alpha*f_ck/gamma_c
+	#steel parameters
+	f_yk = 500
+	f_yd = f_yk/gamma_s
+	E_s = 200000
+
+	if f_ck <= 50:
+		lambd = 0.8
+		eta = 1
+	if f_ck > 50:
+		lambd = 0.8 - (f_ck-50)/400
+		eta = 1 - (f_ck-50)/200
+
+	epsilon_cu3_dict = {"C12/15":0.0035, "C16/20":0.0035, "C20/25":0.0035, "C25/30":0.0035, "C30/37":0.0035, "C35/45":0.0035, "C40/50":0.0035,
+						 "C45/55":0.0035, "C50/60":0.0035, "C55/67":0.0031, "C60/75":0.0029, "C70/85": 0.0027, "C80/95": 0.0026, "C90/105": 0.0026}
+
+	epsilon_cu3 = epsilon_cu3_dict[strength_class]
+
+	a_1 = 0.001*(cover + diameter_s + (quantity11*diameter11+quantity12*diameter12)/(quantity11+quantity12)/2)
+
+	a_2 = 0.001*(cover + diameter_s + diameter11top/2)
+
+	#converting A_s1, A_s2 from [cm2] to [m2]
+	A_s1 = 0.01*0.01*A_s1
+	A_s2 = 0.01*0.01*A_s2
+
+	d = h - a_1
+
+	x_lim = epsilon_cu3*d/(epsilon_cu3 + f_yd/E_s)
+	x_yd_min = epsilon_cu3*a_2/(epsilon_cu3 - f_yd/E_s)
+	x_neg_yd_min = epsilon_cu3*a_2/(epsilon_cu3 + f_yd/E_s)
+	
+	x = 1/lambd*f_yd*(A_s1-A_s2)/(eta*f_cd*b)
+
+	if x < x_yd_min:
+
+		sigma_s1 = f_yd
+
+		A = (epsilon_cu3*E_s*A_s2 - f_yd*A_s1)/(eta*f_cd*b)
+		B = 4*lambd*epsilon_cu3*E_s*A_s2/(eta*f_cd*b)*a_2
+
+		x = 1/lambd*(-A+(A**2+B)**(1/2))/2
+
+		if x < x_neg_yd_min:
+
+			x = 1/lambd*f_yd*(A_s1+A_s2)/(eta*f_cd*b)
+			sigma_s2 = -f_yd
+
+		else:
+			sigma_s2 = epsilon_cu3*E_s*(x-a_2)/x
+
+	else:
+		sigma_s2 = f_yd
+
+		if x <= x_lim:
+			sigma_s1 = f_yd
+
+		else:
+			x = x_lim
+			sigma_s1 = (eta*f_cd*b*lambd*x + f_yd*A_s2)/A_s1
+
+	M_Rd = eta*f_cd*1000*b*lambd*x*(d-0.5*lambd*x) + sigma_s2*1000*A_s2*(d-a_2)
+
+	return M_Rd
+
+
+
+
+
+def deflections(f_ck, width, height):
+
+
+#moment wywołany charakterystycznym obciążeniem całkowitym i charakterystycznym obciążeniem długotrwałym
+
+
+	E_c_eff = E_cm/(1 + phi)
+
+	#do obliczenia:
+	phi = 2.0
+
+	#przyjeto cement N
+	alpha_ds1 = 4
+	alpha_ds2 = 0.12
+	#przyjeto RH=50%
+	RH = 50
+	RH_0 = 100
+	beta_RH = 1.55*(1-(RH/RH_0)**3)
+	f_cm = f_ck + 8
+	f_cm0 = 10
+
+	eps_cd_0 = 0.85*(220 + 110*alpha_ds1)*math.exp(-alpha_ds2*f_cm/f_cm0)*beta_RH*10**(-6)
+
+	A_c = width*10*height*10
+	u = 2*width*10+2*height*10
+	#h_0 in mm
+	h_0 = 2*A_c/u
+
+
+	if h_0 >= 500:
+		k_h = 0.7
+	elif h_0 >= 300:
+		k_h = 0.75 - ((h_0 - 300)/(500-300)*(0.75-0.70))
+	elif h_0 >= 200:
+		k_h = 0.85 - ((h_0 - 200)/(300-200)*(0.85-0.75))
+	elif h_0 >= 100:
+		k_h = 1.0 - ((h_0 - 100)/(200-100)*(1.0-0.85))
+	elif h_0 < 100:
+		k_h = 1.0
+
+
+
+
+	eps_cd = k_h*eps_cd_0
+	eps_ca = 2.5*(f_ck - 10)*10**(-6)
+
+
+	eps_cs = eps_cd + eps_ca
+
+
+	alpha_m = 5/48
+	alpha_cs = 1/8
+
+
+
+
+
+
--- website/views_belka_zelbetowa.py
+++ website/views_belka_zelbetowa.py
@@ -4,7 +4,7 @@ from . import db
 import json
 import math
 from cross_section_img import cross_section_img
-from reinforcement_area_calc import reinf_area_calc
+from reinforcement_area_calc import reinf_area_calc, bending_moment_resistance
 
 views_belka_zelbetowa = Blueprint('views_belka_zelbetowa', __name__)
 
@@ -23,10 +23,6 @@ diameters = [6, 8, 10, 12, 16, 20, 25, 32, 40]
 @login_required
 
 def belka_zelbetowa():
-
-
-	A_s1 = A_s2 = A_0 = A_0_lim = A_smin = A_smax = M_span = M_support = static_scheme = ""
-
 
 	if request.method == 'POST':
 
@@ -47,141 +43,118 @@ def belka_zelbetowa():
 
 		else:
 
-			length = float(length)
-			height = int(height)
-			width = int(width)
-			q_def = float(q_def)
-			diameter_s = int(diameter_s)
-			bottom_def_dia = int(bottom_def_dia)
-			top_def_dia = int(top_def_dia)
-			cover = float(cover)
-
-			f_ck = f_ck_dict[strength_class]
-
-			q_weight = 0.01*height*0.01*width*25
+			return redirect(url_for('views_belka_zelbetowa.belka_zelbetowa_02', static_scheme=static_scheme, length=length, height=height,
+								width=width, q_def=q_def, des_sit=des_sit, strength_class=strength_class, diameter_s=diameter_s,
+								bottom_def_dia=bottom_def_dia, top_def_dia=top_def_dia, cover=cover))
 
 		
-			if static_scheme == "swobodnie podparta":
-				M_span = round((q_def+q_weight)*length**2/8, 3)
-
-			elif static_scheme == "obustronnie utwierdzona":
-				M_support = round(-(q_def+q_weight)*length**2/12, 3)
-				M_span = round((q_def+q_weight)*length**2/24, 3)
-
-
-			A_s1, A_s2, A_0, A_0_lim, A_smin, A_smax = reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover, diameter_s, bottom_def_dia, top_def_dia)
-
-			A_s1 = round(A_s1, 2)
-			A_s2 = round(A_s2, 2)
-			A_smin = round(A_smin, 2)
+	return render_template("belka_zelbetowa.html", user=current_user, strength_classes=strength_classes, diameters=diameters)
 
 
 
-		# diameter11 = request.form.get('diameter11')
-		# quantity11 = request.form.get('quantity11')
-		# diameter12 = request.form.get('diameter12')
-		# quantity12 = request.form.get('quantity12')
-		# diameter11top = request.form.get('diameter11top')
-		# quantity11top = request.form.get('quantity11top')
+@views_belka_zelbetowa.route('/belka_zelbetowa_02', methods=['GET', 'POST'])
+@login_required
+
+def belka_zelbetowa_02():
 
 
+	M_Rd = A_s1 = A_s2 = A_s1_req = A_s2_req = A_0 = A_0_lim = A_smin = A_smax = M_span = M_support = static_scheme = ""
+
+	static_scheme = request.args.get('static_scheme')
+	length = float(request.args.get('length'))
+	height = int(request.args.get('height'))
+	width = int(request.args.get('width'))
+	q_def = float(request.args.get('q_def'))
+	des_sit = request.args.get('des_sit')
+	strength_class = request.args.get('strength_class')
+	diameter_s = int(request.args.get('diameter_s'))
+	bottom_def_dia = int(request.args.get('bottom_def_dia'))
+	top_def_dia = int(request.args.get('top_def_dia'))
+	cover = int(request.args.get('cover'))
 
 
+	f_ck = f_ck_dict[strength_class]
+
+	q_weight = 0.01*height*0.01*width*25
 
 
+	if static_scheme == "swobodnie podparta":
+		M_span = round((q_def+q_weight)*length**2/8, 3)
+
+	elif static_scheme == "obustronnie utwierdzona":
+		M_support = round(-(q_def+q_weight)*length**2/12, 3)
+		M_span = round((q_def+q_weight)*length**2/24, 3)
 
 
-			# return redirect(url_for('views_belka_zelbetowa.belka_zelbetowa_02', static_scheme=static_scheme, length=length, q_def=q_def, height=height,
-			# 					width=width, des_sit=des_sit))
-
-		
-	return render_template("belka_zelbetowa.html", user=current_user, strength_classes=strength_classes,
-							diameters=diameters, A_s1=A_s1, A_s2=A_s2, A_0=A_0, A_0_lim=A_0_lim, A_smin=A_smin,
-							A_smax=A_smax, M_span=M_span, M_support=M_support, static_scheme=static_scheme)
+	A_s1_req, A_s2_req, A_0, A_0_lim, A_smin, A_smax = reinf_area_calc(M_span, width, height, des_sit, strength_class, f_ck, cover, diameter_s, bottom_def_dia, top_def_dia)
 
 
-
-# @views_belka_zelbetowa.route('/belka_zelbetowa_02', methods=['GET', 'POST'])
-# @login_required
-
-# def belka_zelbetowa_02():
+	#include algorithm which suggests rebar 
 
 
-# 	# static_scheme = request.args.get('static_scheme')
-# 	# length = float(request.args.get('length'))
-# 	# q_def = float(request.args.get('q_def'))
-# 	# height = int(request.args.get('height'))
-# 	# width = int(request.args.get('width'))
-# 	# des_sit = request.args.get('des_sit')
+	A_s1_req = round(A_s1_req, 2)
+	A_s2_req = round(A_s2_req, 2)
+	A_smin = round(A_smin, 2)
 
 
+	if request.method == 'POST':
 
-# 	if request.method == 'POST':
-
-# 		diameter11 = request.form.get('diameter11')
-# 		quantity11 = request.form.get('quantity11')
-# 		diameter12 = request.form.get('diameter12')
-# 		quantity12 = request.form.get('quantity12')
-# 		diameter11top = request.form.get('diameter11top')
-# 		quantity11top = request.form.get('quantity11top')
+		diameter11 = int(request.form.get('diameter11'))
+		quantity11 = int(request.form.get('quantity11'))
+		diameter12 = int(request.form.get('diameter12'))
+		quantity12 = int(request.form.get('quantity12'))
+		diameter11top = int(request.form.get('diameter11top'))
+		quantity11top = int(request.form.get('quantity11top'))
 		
 
-
-# 		if 0 in [len(cover)]:
-# 			flash('Uzupełnij dane.')
-
-# 		else:
-
-# 			diameter11 = int(diameter11)
-# 			quantity11 = int(quantity11)
-# 			diameter12 = int(diameter12)
-# 			quantity12 = int(quantity12)
-# 			diameter11top = int(diameter11top)
-# 			quantity11top = int(quantity11top)
+		if (quantity11%2 == 1 and quantity12%2 == 1):
+			flash('Liczby sztuk obu typów prętów nie mogą być obie nieparzyste (brak symetrii zbrojenia w przekroju).')
 
 
-# 			if (quantity11%2 == 1 and quantity12%2 == 1):
-# 				flash('Liczby sztuk obu typów prętów nie mogą być obie nieparzyste (brak symetrii zbrojenia w przekroju).')
+		width_rebar_mm = width*10 - 2*cover - 2*diameter_s
+		rebar_spacing = (width_rebar_mm - quantity11*diameter11 - quantity12*diameter12)/(quantity11 + quantity12 - 1)
+
+		d_g = 16
+		k_1 = 1
+		k_2 = 5
 
 
-# 			width_rebar_mm = width*10 - 2*cover - 2*diameter_s
-# 			rebar_spacing = (width_rebar_mm - quantity11*diameter11 - quantity12*diameter12)/(quantity11 + quantity12 - 1)
+		if rebar_spacing < min(diameter11, diameter12):
+			flash('Rozstaw prętów musi być większy niż największa z przyjętych średnic pręta.')
 
-# 			d_g = 16
-# 			k_1 = 1
-# 			k_2 = 5
+		elif rebar_spacing < (d_g + k_2):
+			flash('Rozstaw prętów musi być większy niż średnica kruszywa powiększona o 5mm (przyjęta średnica kruszywa: 16mm).')
+
+		elif rebar_spacing < 20:
+			flash('Rozstaw prętów musi być większy niż 20mm.')
+
+		else:
+			cross_section_img(diameter11, diameter12, diameter11top, quantity11, quantity12, quantity11top, cover, diameter_s, height, width)
+
+			A_s1 = 0.01*3.14159*(quantity11*diameter11**2/4 + quantity12*diameter12**2/4)
+			A_s2 = 0.01*3.14159*quantity11top*diameter11top**2/4
+
+			M_Rd = bending_moment_resistance(M_span, width, height, des_sit, strength_class, f_ck, cover, diameter_s, diameter11, quantity11, diameter12, quantity12,
+											diameter11top, quantity11top, A_s1, A_s2)
 
 
-# 			if rebar_spacing < min(diameter11, diameter12):
-# 				flash('Rozstaw prętów musi być większy niż największa z przyjętych średnic pręta.')
+			as1 = round(0.01*3.14159*(quantity11*diameter11**2/4 + quantity12*diameter12**2/4), 2)
 
-# 			elif rebar_spacing < (d_g + k_2):
-# 				flash('Rozstaw prętów musi być większy niż średnica kruszywa powiększona o 5mm (przyjęta średnica kruszywa: 16mm).')
-
-# 			elif rebar_spacing < 20:
-# 				flash('Rozstaw prętów musi być większy niż 20mm.')
-
-# 			else:
-# 				cross_section_img(diameter11, diameter12, diameter11top, quantity11, quantity12, quantity11top, cover, diameter_s, height, width)
-
-# 				as1 = round(0.01*3.14159*(quantity11*diameter11**2/4 + quantity12*diameter12**2/4), 2)
-
-# 				f = open("Wyniki_belka_zelbetowa.txt", "w", encoding="utf-8")
-# 				f.write(f"Przyjęta powierzchnia zbrojenia dolnego wynosi: {as1}cm2")
-# 				f.close()
+			f = open("Wyniki_belka_zelbetowa.txt", "w", encoding="utf-8")
+			f.write(f"Przyjęta powierzchnia zbrojenia dolnego wynosi: {as1}cm2")
+			f.close()
 
 
 
-
-# 	return render_template("belka_zelbetowa_02.html", user=current_user, static_scheme=static_scheme, M_span=M_span, M_support=M_support, length=length,
-# 							height=height, q_def=q_def, width=width, strength_classes=strength_classes, f_ck_dict=f_ck_dict, diameters=diameters, des_sit=des_sit)
+	return render_template("belka_zelbetowa_02.html", user=current_user, static_scheme=static_scheme, M_span=M_span, M_support=M_support, length=length,
+							height=height, q_def=q_def, width=width, f_ck_dict=f_ck_dict, diameters=diameters, des_sit=des_sit, A_s1_req=A_s1_req, A_s2_req=A_s2_req,
+							A_0=A_0, A_0_lim=A_0_lim, A_smin=A_smin, A_smax=A_smax, A_s1=A_s1, A_s2=A_s2, M_Rd=M_Rd)
 
 
 
 
+@views_belka_zelbetowa.route('/belka_zelbetowa_wyniki/')
+@login_required
 
-# @views_belka_zelbetowa.route('/belka_zelbetowa_wyniki/')
-# @login_required
-
-# def belka_zelbetowa_wyniki():
-# 	return send_file('C:\\Users\\konie\\OneDrive\\STUFF\\CODING\\01 ZAKOTWIENIE PRĘTÓW\\website\\static\\Wyniki_belka_zelbetowa.txt', as_attachment=True)
+def belka_zelbetowa_wyniki():
+	return send_file('C:\\Users\\konie\\OneDrive\\STUFF\\CODING\\01 ZAKOTWIENIE PRĘTÓW\\website\\static\\Wyniki_belka_zelbetowa.txt', as_attachment=True)
